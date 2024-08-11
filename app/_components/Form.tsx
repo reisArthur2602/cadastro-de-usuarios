39e6ce1e638009553cb94b8842f3c1a8c8713ca1
@dev.arthur <contato.arthurreis222@gmail.com>
2024-08-11 18:12:27 -0300

    feat: add form components

---
+++ app/_components/Form.tsx
@@ -0,0 +1,57 @@
+import {
+  ButtonHTMLAttributes,
+  FormHTMLAttributes,
+  InputHTMLAttributes,
+  LabelHTMLAttributes,
+} from 'react';
+
+const Form = ({ children, ...props }: FormHTMLAttributes<HTMLFormElement>) => {
+  return (
+    <form {...props} className="flex items-center gap-4">
+      {children}
+    </form>
+  );
+};
+
+export const Field = ({ ...props }: InputHTMLAttributes<HTMLInputElement>) => {
+  return (
+    <input
+      {...props}
+      className="bg-transparent w-full outline-none placeholder:text-indigo-400 text-indigo-800"
+    />
+  );
+};
+
+export const Label = ({
+  children,
+  ...props
+}: LabelHTMLAttributes<HTMLLabelElement>) => {
+  return (
+    <label
+      {...props}
+      className="w-full bg-indigo-100 p-4 rounded-[0.25rem] text-indigo-400 flex items-center gap-2"
+    >
+      {children}
+    </label>
+  );
+};
+
+export const Submit = ({
+  children,
+  ...props
+}: ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+      {...props}
+      className="bg-indigo-600 py-4 px-8 rounded-[0.25rem] text-indigo-50 flex items-center justify-center gap-2 hover:animate-pulse duration-100 transition-all"
+    >
+      {children}
+    </button>
+  );
+};
+
+Form.Label = Label;
+Form.Field = Field;
+Form.Submit = Submit;
+
+export { Form };
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Form } from './_components';
+import { AtSign, Phone, User } from 'lucide-react';
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main className="max-w-7xl w-full p-3 mx-auto mt-10 flex flex-col gap-10">
+      <h1 className="text-center font-bold text-xl sm:text-3xl">
+        Lista de Contatos
+      </h1>
+      <Form>
+        <Form.Label>
+          <User size={25} />
+          <Form.Field placeholder="Nome Completo" />
+        </Form.Label>
 
-export default Home
+        <Form.Label>
+          <AtSign size={25} />
+          <Form.Field placeholder="Nome Completo" />
+        </Form.Label>
+
+        <Form.Label>
+          <Phone size={25} />
+          <Form.Field placeholder="Nome Completo" />
+        </Form.Label>
+        <Form.Submit>Salvar</Form.Submit>
+      </Form>
+    </main>
+  );
+};
+
+export default Home;
